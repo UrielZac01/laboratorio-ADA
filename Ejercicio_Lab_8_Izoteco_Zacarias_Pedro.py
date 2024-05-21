2b5833a9057455f11a493841cdbbc8dbe6ad4229
Pedro Uriel Izoteco Zacarias <144086979+UrielZac01@users.noreply.github.com>
2024-05-20 23:45:07 -0600

    Ejercicio de Laboratorio 9

--- Ejercicio_Lab_8_Izoteco_Zacarias_Pedro.py
+++ Ejercicio_Lab_8_Izoteco_Zacarias_Pedro.py
@@ -1,25 +1,48 @@
-def cont_formas(n):
-    # Casos base
-    if n == 0:
-        return 1
-    elif n == 1:
-        return 1
-    elif n == 2:
-        return 2
-
-    formas = [0] * (n + 1)
-
-    # Arreglos para casos base
-    formas[0] = 1
-    formas[1] = 2
-    formas[2] = 3
-
-    # Calculamos el numero de formas posibles para cada peldaño
-    for i in range(3, n + 1):
-        formas[i] = formas[i - 1] + formas[i - 2] + formas[i - 3]
-
-    return formas[n]
+#Solucion para valores distintos
+def find_magic_index_distinct(arr):
+    def binary_search(arr, left, right):
+        if left > right:
+            return -1
+        
+        mid = (left + right) // 2
+        if arr[mid] == mid:
+            return mid
+        elif arr[mid] > mid:
+            return binary_search(arr, left, mid - 1)
+        else:
+            return binary_search(arr, mid + 1, right)
+    
+    return binary_search(arr, 0, len(arr) - 1)
 
 # Ejemplo de uso
-n = int(input("Ingrese el numero de peldaños: "))
-print("Numero de formas posibles de subir la escalera de", n, "peldaños:",cont_formas(n))
+arr = [10, 5,2, 4, 3]
+print(find_magic_index_distinct(arr))  # Salida: 2, arr[2]=2
+
+
+#Solucion para valores NO distintos
+def find_magic_index_non_distinct(arr):
+    def binary_search(arr, left, right):
+        if left > right:
+            return -1
+        
+        mid = (left + right) // 2
+        mid_value = arr[mid]
+        
+        if mid == mid_value:
+            return mid
+        
+        # Buscar en la mitad izquierda
+        left_index = min(mid - 1, mid_value)
+        left_result = binary_search(arr, left, left_index)
+        if left_result != -1:
+            return left_result
+        
+        # Buscar en la mitad derecha
+        right_index = max(mid + 1, mid_value)
+        return binary_search(arr, right_index, right)
+    
+    return binary_search(arr, 0, len(arr) - 1)
+
+# Ejemplo de uso
+arr = [1, 8, 2, 2, 2, 3, 4, 7, 9, 12, 13]
+print(find_magic_index_non_distinct(arr))  # Salida: 7 ya que arr[7]=7
